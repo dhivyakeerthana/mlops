0f5a6ee22ef8f37ba39ee0f01262a7d5f0df0597
dhivyakeerthana <dhivyakeerthanas@gmail.com>
2021-09-22 19:43:04 +0530

    added test cases

--- test_assignment.py
+++ test_assignment.py
@@ -1,3 +1,13 @@
-def test_capitalize_string():
-    abc = 'Test'
-    assert abc == 'Test'
+import os
+from pathlib import Path
+
+def check_if_data_exits():
+    print(os.path.isdir('data'))
+    assert os.path.isdir('data') == True
+
+def check_if_model_exits():
+    print(os.path.isfile('model.h5'))
+    assert os.path.isfile('model.h5') == True
+
+check_if_data_exits()
+check_if_model_exits()
