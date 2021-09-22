a447c352997be41946b1671a1484de26d046bff2
Dhivya Keerthana <77927724+dhivyakeerthana@users.noreply.github.com>
2021-09-22 19:53:10 +0530

    Update test_assignment.py

--- test_assignment.py
+++ test_assignment.py
@@ -4,10 +4,10 @@ def test_capitalize_string():
     abc = 'Test'
     assert abc == 'Test'
 
-def check_if_data_exits():
+def test_if_data_exits():
     print(os.path.isdir('data'))
     assert os.path.isdir('data') == True
 
-def check_if_model_exits():
+def test_if_model_exits():
     print(os.path.isfile('model.h5'))
     assert os.path.isfile('model.h5') == True
