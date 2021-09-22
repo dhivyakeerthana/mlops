260bd422667a2c5ea219a4443e432455c0eed7dd
Dhivya Keerthana <77927724+dhivyakeerthana@users.noreply.github.com>
2021-09-22 19:50:09 +0530

    Corrected test cases

--- test_assignment.py
+++ test_assignment.py
@@ -1,3 +1,9 @@
-def test_capitalize_string():
-    abc = 'Test'
-    assert abc == 'Test'
+import os
+
+def check_if_data_exits():
+    print(os.path.isdir('data'))
+    assert os.path.isdir('data') == True
+
+def check_if_model_exits():
+    print(os.path.isfile('model.h5'))
+    assert os.path.isfile('model.h5') == True
