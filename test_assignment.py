8d51ed1ca85e2248362fd30e4a1485a96335723b
Dhivya Keerthana <77927724+dhivyakeerthana@users.noreply.github.com>
2021-09-22 19:51:53 +0530

    Update test_assignment.py

--- test_assignment.py
+++ test_assignment.py
@@ -1,5 +1,9 @@
 import os
 
+def test_capitalize_string():
+    abc = 'Test'
+    assert abc == 'Test'
+
 def check_if_data_exits():
     print(os.path.isdir('data'))
     assert os.path.isdir('data') == True
