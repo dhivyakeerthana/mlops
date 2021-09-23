0d12148ed1ccac73489e790d00581ebbca852fbf
Dhivya Keerthana <77927724+dhivyakeerthana@users.noreply.github.com>
2021-09-23 17:19:52 +0530

    Update py_api.py

--- py_api.py
+++ py_api.py
@@ -1,4 +1,8 @@
-import dvc.api
-with dvc.api.open("metrics.csv", repo="https://github.com/dhivyakeerthana/mlops") as f:
+# import dvc.api
+# with dvc.api.open("metrics.csv", repo="https://github.com/dhivyakeerthana/mlops") as f:
+#     content = f.read()
+#     print(content)
+
+with open("metrics.csv") as f:
     content = f.read()
     print(content)
